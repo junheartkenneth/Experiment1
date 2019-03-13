933927e6102f82cc72791410b0a486f91d253679
junheartkenneth <47804370+junheartkenneth@users.noreply.github.com>
2019-03-13 12:29:30 +0800

    Add files via upload

---
+++ Experiment1-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+using namespace std;
+int main()
+{
+
+int density;
+int mass;
+int volume;
+volume = (density * (mass / 4)) / volume;
+
+cout << "Mass" << endl;
+cin >> mass;
+cout << "density" << endl;
+cin >> density;
+cout << "Volume:" << volume << endl;
+
+	_getch();
+	return 0;
+}
